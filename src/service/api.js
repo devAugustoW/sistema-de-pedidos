56da5ae6e72f57312c1146a6213cdab29c0a89f8
Augusto Dantas <augustodantas.dev@gmail.com>
2024-03-13 20:05:25 -0300

    new URL_SERVER

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL_SERVER = "http://localhost:3001";
+const URL_SERVER = "https://sitema-de-pedidos-backend.onrender.com/";
 
 const api = axios.create({
    baseURL: URL_SERVER
